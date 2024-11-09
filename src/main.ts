12bd401e29a959cf1e10f4c21609b896d7e2cfe8
olusoga olalekan <olusogaolamileksy@gmail.com>
2024-11-09 19:55:58 +0100

    edit swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,11 @@ async function bootstrap() {
     .build();
 
   const swaggerDocument = SwaggerModule.createDocument(app, options);
+
+  app.use('/support-json', (req, res) => {
+    res.json(swaggerDocument);
+  });
+  
   SwaggerModule.setup('/support', app, swaggerDocument, {
     customCss:
       '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
