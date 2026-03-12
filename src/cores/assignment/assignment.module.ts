54a18c33928f64bef1ba3f2550165565f23397b0
deejaycodes20 <deji@motorway.co.uk>
2026-03-12 09:53:14 +0100

    Fix: Add missing IncidentType entity to AssignmentModule
    ERROR: Nest can't resolve dependencies of UsersRepository
    CAUSE: UsersRepository needs IncidentType but AssignmentModule didn't import it
    
    FIX: Add IncidentType to TypeOrmModule.forFeature in AssignmentModule
    
    This fixes the Render deployment error.

--- src/cores/assignment/assignment.module.ts
+++ src/cores/assignment/assignment.module.ts
@@ -5,9 +5,10 @@ import { UsersRepository } from 'src/basics/users/users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Report } from 'src/common/entities/report.entity';
 import { User } from 'src/common/entities/user.entity';
+import { IncidentType } from 'src/common/entities/incident-type.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Report, User])],
+  imports: [TypeOrmModule.forFeature([Report, User, IncidentType])],
   providers: [AssignmentService, ReportsRepository, UsersRepository],
   exports: [AssignmentService],
 })
